c8b22af42f37985172e6461a3d2bda6e6b3302d5
Wahdan <wahdan@Ahmed.wahdan>
2019-02-12 11:04:05 +0200

    Add simple computer vision algorithm to detect red traffic lights

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,5 +1,6 @@
 from styx_msgs.msg import TrafficLight
-
+import cv2
+import rospy
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
@@ -15,5 +16,30 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Threshold the HSV image, keep only the red pixels
+        red = cv2.inRange(hsv_image, (0, 100, 100), (5, 255, 255))
+
+        red = cv2.GaussianBlur(red, (9, 9), 2, 2)
+
+        # Use the Hough transform to detect circles in the combined threshold image
+        red_circles = cv2.HoughCircles(red, cv2.HOUGH_GRADIENT, 1, image.shape[0] / 8.0, 100, 50, 12, 0)
+
+
+
+        if red_circles.shape:
+            #print(red_circles.shape)
+            #print("red circles ",len(red_circles[0,:]))  
+            if(len(red_circles[0,:]) >= 2):
+                rospy.loginfo("Red traffic light detected")
+                return TrafficLight.RED
+            else:
+                return TrafficLight.UNKNOWN
+                
+
+
+
+        
+
         return TrafficLight.UNKNOWN
